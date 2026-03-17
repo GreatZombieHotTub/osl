fe17e53747456ef1c1896ec81112f28be17a7311
GreatZombieHotTub <94970958+GreatZombieHotTub@users.noreply.github.com>
2026-03-17 20:34:53 +0530

    Update producer_shm.c

--- week6/producer_shm.c
+++ week6/producer_shm.c
@@ -4,7 +4,14 @@
 #include <string.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
-#include "shm_words.h"
+// shm_words.h
+#define TEXT_SZ 100
+
+struct shared_data {
+    int written;
+    char text[TEXT_SZ];
+};
+
 
 int main() {
     int shmid;
