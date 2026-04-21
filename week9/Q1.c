8c11c4eab386723823b1df7ff1ed32dcf6bb5084
GreatZombieHotTub <94970958+GreatZombieHotTub@users.noreply.github.com>
2026-04-21 16:19:57 +0530

    Refactor memory allocation functions and main logic

--- week9/Q1.c
+++ week9/Q1.c
@@ -1,83 +1,90 @@
 #include <stdio.h>
-#include <string.h>
 
 #define MAX 10
-void firstFit(int blocks[], int nb, int procs[], int np) {
-    int alloc[MAX];
-    int temp[MAX];
-    memset(alloc, -1, sizeof(alloc));
-    for (int i = 0; i < nb; i++) temp[i] = blocks[i];
-
-    printf("\n--- First Fit ---\n");
-    for (int i = 0; i < np; i++) {
-        for (int j = 0; j < nb; j++) {
-            if (temp[j] >= procs[i]) {
-                alloc[i] = j;
-                temp[j] -= procs[i];
-                break;
-            }
-        }
-        if (alloc[i] != -1)
-            printf("Process %d (%dK) -> Block %d (%dK)\n", i+1, procs[i], alloc[i]+1, blocks[alloc[i]]);
-        else
-            printf("Process %d (%dK) -> Not Allocated\n", i+1, procs[i]); }}
-
-void bestFit(int blocks[], int nb, int procs[], int np) {
-    int alloc[MAX];
-    int temp[MAX];
-    memset(alloc, -1, sizeof(alloc));
-    for (int i = 0; i < nb; i++) temp[i] = blocks[i];
-    printf("\n--- Best Fit ---\n");
-    for (int i = 0; i < np; i++) {
-        int best = -1;
-        for (int j = 0; j < nb; j++) {
-            if (temp[j] >= procs[i]) {
-                if (best == -1 || temp[j] < temp[best])
-                    best = j;
-            }}
-        alloc[i] = best;
-        if (best != -1) {
-            temp[best] -= procs[i];
-            printf("Process %d (%dK) -> Block %d (%dK)\n", i+1, procs[i], best+1, blocks[best]);
-        } else {
-            printf("Process %d (%dK) -> Not Allocated\n", i+1, procs[i]); }}}
-
-void worstFit(int blocks[], int nb, int procs[], int np) {
-    int alloc[MAX];
-    int temp[MAX];
-    memset(alloc, -1, sizeof(alloc));
-    for (int i = 0; i < nb; i++) temp[i] = blocks[i];
-
-    printf("\n--- Worst Fit ---\n");
-    for (int i = 0; i < np; i++) {
-        int worst = -1;
-        for (int j = 0; j < nb; j++) {
-            if (temp[j] >= procs[i]) {
-                if (worst == -1 || temp[j] > temp[worst])
-                    worst = j;
-            }}
-        alloc[i] = worst;
-        if (worst != -1) {
-            temp[worst] -= procs[i];
-            printf("Process %d (%dK) -> Block %d (%dK)\n", i+1, procs[i], worst+1, blocks[worst]);} 
-        else {
-            printf("Process %d (%dK) -> Not Allocated\n", i+1, procs[i]);
-        }}}
 
 int main() {
     int blocks[] = {100, 500, 200, 300, 600};
     int procs[]  = {212, 417, 112, 426};
     int nb = 5, np = 4;
 
-    printf("Memory Blocks (K): ");
-    for (int i = 0; i < nb; i++) printf("%d ", blocks[i]);
-    printf("\nProcesses (K):     ");
-    for (int i = 0; i < np; i++) printf("%d ", procs[i]);
+    int temp[MAX], alloc[MAX];
 
-    firstFit(blocks, nb, procs, np);
-    bestFit(blocks, nb, procs, np);
-    worstFit(blocks, nb, procs, np);
+    printf("Blocks: ");
+    for(int i = 0; i < nb; i++) printf("%d ", blocks[i]);
 
-    printf("\nConclusion: Best-fit makes most efficient use of memory by minimizing leftover holes.\n");
+    printf("\nProcs:  ");
+    for(int i = 0; i < np; i++) printf("%d ", procs[i]);
+
+    // ---------- FIRST FIT ----------
+    for(int i = 0; i < nb; i++) temp[i] = blocks[i];
+    for(int i = 0; i < np; i++) alloc[i] = -1;
+
+    printf("\n\nFirst Fit:\n");
+
+    for(int i = 0; i < np; i++) {
+        for(int j = 0; j < nb; j++) {
+            if(temp[j] >= procs[i]) {
+                alloc[i] = j;
+                temp[j] -= procs[i];
+                break;
+            }
+        }
+
+        if(alloc[i] != -1)
+            printf("P%d -> B%d\n", i+1, alloc[i]+1);
+        else
+            printf("P%d -> NA\n", i+1);
+    }
+
+    // ---------- BEST FIT ----------
+    for(int i = 0; i < nb; i++) temp[i] = blocks[i];
+    for(int i = 0; i < np; i++) alloc[i] = -1;
+
+    printf("\nBest Fit:\n");
+
+    for(int i = 0; i < np; i++) {
+        int best = -1;
+
+        for(int j = 0; j < nb; j++) {
+            if(temp[j] >= procs[i]) {
+                if(best == -1 || temp[j] < temp[best])
+                    best = j;
+            }
+        }
+
+        if(best != -1) {
+            alloc[i] = best;
+            temp[best] -= procs[i];
+            printf("P%d -> B%d\n", i+1, best+1);
+        } else {
+            printf("P%d -> NA\n", i+1);
+        }
+    }
+
+    // ---------- WORST FIT ----------
+    for(int i = 0; i < nb; i++) temp[i] = blocks[i];
+    for(int i = 0; i < np; i++) alloc[i] = -1;
+
+    printf("\nWorst Fit:\n");
+
+    for(int i = 0; i < np; i++) {
+        int worst = -1;
+
+        for(int j = 0; j < nb; j++) {
+            if(temp[j] >= procs[i]) {
+                if(worst == -1 || temp[j] > temp[worst])
+                    worst = j;
+            }
+        }
+
+        if(worst != -1) {
+            alloc[i] = worst;
+            temp[worst] -= procs[i];
+            printf("P%d -> B%d\n", i+1, worst+1);
+        } else {
+            printf("P%d -> NA\n", i+1);
+        }
+    }
+
     return 0;
 }
